1905096ef21c7fee02b086dd2bc82e7681bead39
JORGE <jogabell@hotmail.com>
2018-08-26 00:15:59 -0500

    Se crea y se prueba la subroutine mixingMatrix

--- elements_oscillation_model/mixing_matrix.f90
+++ elements_oscillation_model/mixing_matrix.f90
@@ -1,23 +1,21 @@
 subroutine mixingMatrix(U,t12,t23,t13,delta)
     implicit none
-    real(8) :: U                  ! U is the mixing matrix of the oscillation model
+    real(8) :: U(3,3)             ! U is the mixing matrix of the oscillation model
     real(8) :: t12,t23,t13        ! Are the mixing angles of hte oscillation model
     real(8) :: delta              ! delta is the phase factor for CP violations
 
     real(8) :: s12,s23,s13        ! Are the sin's os the mixing angles of the oscillation model
     real(8) :: c12,c23,c13        ! Are the cos's os the mixing angles of the oscillation model
-    double complex :: arg,exp_delta
-    real(8) :: U(3,3)
-    
+    double complex :: arg,exp_delta   
 
     s12=sin(t12); s23=sin(t23); s13=sin(t13)
     c12=cos(t12); c23=cos(t23); c13=cos(t13)
 
     arg=cmplx(0.0,-delta)
-    exp_delta=exp(arg)    
+    exp_delta=exp(arg)
 
-    U(1,1)=c13*c12;                            U(1,2)=c13*s12;                        U(1,3)=s13*exp(cmplx(0.0,-delta))
-    U(2,1)=-s12*c23-c12*s23*s13*exp(cmplx(0.0,delta)); U(2,2)=c12*c23-s12*s23*s13*exp(cmplx(0.0,delta));  U(2,3)=s23*c13
-    U(3,1)=s12*s23-c12*c23*s13*exp(cmplx(0.0,delta));  U(3,2)=-c12*s23-s12*c23*s13*exp(cmplx(0.0,delta)); U(3,3)=c23*c13    
+    U(1,1)=c13*c12;                            U(1,2)=c13*s12;                        U(1,3)=s13*exp(cmplx(0.0d0,-delta))
+    U(2,1)=-s12*c23-c12*s23*s13*exp(cmplx(0.0d0,delta)); U(2,2)=c12*c23-s12*s23*s13*exp(cmplx(0.0d0,delta));  U(2,3)=s23*c13
+    U(3,1)=s12*s23-c12*c23*s13*exp(cmplx(0.0d0,delta));  U(3,2)=-c12*s23-s12*c23*s13*exp(cmplx(0.0d0,delta)); U(3,3)=c23*c13    
     return
 end subroutine mixingMatrix
--- solar_neutrinos.f90
+++ solar_neutrinos.f90
@@ -1,50 +1,26 @@
 program solar_neutrino
     implicit none
-    real(8) :: val1, val2, PI
+    real(8), parameter :: PI=3.141592653589793238462643d0
+    real(8) :: val1, val2
     integer :: i,j,k
     double complex :: a,b
-
-    real(8) :: mixing_matrix
-    real(8) :: t12,t23,t13,delta
-    real(8) :: matrixA(3,3), matrixB(3,3), result(3,3)
+    real(8) :: U(3,3)
+    real(8) :: t12,t23,t13,delta   
     integer :: nu                 ! nu is 1 for neutrinos an 2 for antineutrino
     real(8) :: Ne                 ! Ne is the electron density
     real(8) :: T(3,3)
-    PI=3.141592653589793238462643d0
+    
+    t12=0.0d0
+    t23=0.0d0
+    t13=PI/2.0d0
+    delta=1.0d0
+    call mixingMatrix(U,t12,t23,t13,delta)
 
-    nu=1
-    Ne=0.0d0
-
-    call tMatrix(T,nu,Ne)
-
-    print*,'T(1,:)'
-    print*,T(1,:)
-    print*,T(2,:)
-    print*,T(3,:)
-
-    !matrixA(1,1)=1;matrixA(1,2)=1;matrixA(1,3)=1
-    !matrixA(2,1)=1;matrixA(2,2)=1;matrixA(2,3)=1
-    !matrixA(3,1)=1;matrixA(3,2)=1;matrixA(3,3)=1
-
-
-    !matrixB = matrixA
-    !result = MATMUL(matrixA, matrixB)
-    !print*,'Matrix example'
-    !print*, result
-
-
-
-    !t12=0.0
-    !t23=0.0
-    !t13=PI/2.0
-    !delta=0.0
-
-    !val1 = mixing_matrix(1,1,t12,t23,t13,delta)    
-
-    !print*, 'In solar_neutrino '
-    !print*, val1
-   
-    !call mixing_matrix(val1, val2)
-
+    print*, U(1,:)
+    print*, U(2,:)
+    print*, U(3,:)
+    !nu=1
+    !Ne=0.0d0
+    !call tMatrix(T,nu,Ne)
 
 end program solar_neutrino
