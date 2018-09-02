6d5f99242daa7bd9fe8f275e7e969b1169dbd93c
JORGE <jogabell@hotmail.com>
2018-09-02 16:43:05 -0500

    Se crea funcion que retorna los coeficientes C de l polinomio característico de la matriz T

---
+++ elements_oscillation_model/coefficient_c.f90
@@ -0,0 +1,26 @@
+real(8) function coefficientC(Ci,t12,t23,t13,delta,m1,m2,m3,P,nu,Ne)
+    implicit none
+    integer :: Ci                 ! Ci is the number of coeffienct C of the characteristic polynomial of matrix T 
+    real(8) :: T(3,3)             ! T is a traceless matrix from the model
+    real(8) :: t12,t23,t13,delta  ! Are the three mixing angles and the CP-violation phase of the mixing matrix
+    real(8) :: m1,m2,m3           ! m1,m2,m3 are the masses of the mass-eogenstates
+    real(8) :: P                  ! P es el momento del neutrino
+    integer :: nu                 ! nu is 1 for neutrinos an 2 for antineutrino    
+    real(8) :: Ne                 ! Ne is the electron density
+
+    select case(Ci)
+        case(0)
+            call tMatrix(T,t12,t23,t13,delta,m1,m2,m3,P,nu,Ne)
+            coefficientC = -(   T(1,1)+T(2,2)+T(3,3)  )
+        case(1)
+            call tMatrix(T,t12,t23,t13,delta,m1,m2,m3,P,nu,Ne)
+            coefficientC =  T(1,1)*T(2,2)-T(1,2)*T(2,1) &
+                           +T(1,1)*T(3,3)-T(1,3)*T(3,1) &
+                           +T(2,2)*T(3,3)-T(2,3)*T(3,2)
+        case(2)
+            coefficientC = 0.0d0
+        case DEFAULT
+            coefficientC = 0.0d0
+    end select    
+    return
+end function coefficientC
--- solar_neutrinos.f90
+++ solar_neutrinos.f90
@@ -14,6 +14,7 @@ program solar_neutrino
     real(8) :: T1(3,3)
     real(8) :: T2(3,3)
     real(8) :: T3(3,3)
+    real(8) :: T(3,3)
     real(8) :: complexPhaseFactor
     real(8) :: L
     
@@ -21,16 +22,19 @@ program solar_neutrino
     t23=PI/4.0d0
     t13=PI/4.0d0
     delta=1.0d0
-    m1=(PI/4.0d0)*1.0d-3
-    m2=(PI/4.0d0)*1.0d-3
-    m1=(PI/4.0d0)*1.0d-3
-    L=30.0d3
-    P=1.0d6
-    nu=2.0d0
-    Ne=1.98d+28
+    m1=(PI/4.0d0)*1.0E-3
+    m2=(PI/4.0d0)*1.0E-3
+    m1=(PI/4.0d0)*1.0E-3
+    L=30.0E3
+    P=1.0E6
+    nu=2.0E0
+    Ne=1.98E+30
     
-
-    print*, 'EStoy en el main program'
-    print *, complexPhaseFactor(t12,t23,t13,delta,m1,m2,m3,L,P,nu,Ne)
+    call tMatrix(T,t12,t23,t13,delta,m1,m2,m3,P,nu,Ne)
+    print*, 'EStoy en el main program  tMatrix'
+    print*, T(1,:)
+    print*, T(2,:)
+    print*, T(3,:)
+    print*, T(1,1)+T(2,2)+T(3,3)
     print*, 'EStoy en el main program'
 end program solar_neutrino
