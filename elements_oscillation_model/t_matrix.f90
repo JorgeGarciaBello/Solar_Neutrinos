ef6fadc88f8b56bf2ab4b107fbd726d74ac03637
JORGE <jogabell@hotmail.com>
2018-09-02 15:36:49 -0500

    Creación de la matriz T

--- elements_oscillation_model/t_matrix.f90
+++ elements_oscillation_model/t_matrix.f90
@@ -4,23 +4,23 @@
 !       the T-matrix (3,3)
 !
 !################################################
-subroutine tMatrix(T,nu,Ne)
+subroutine tMatrix(T,t12,t23,t13,delta,m1,m2,m3,P,nu,Ne)
     implicit none
     real(8) :: T(3,3)             ! T is a traceless matrix from the model
-    integer :: nu                 ! nu is 1 for neutrinos an 2 for antineutrino
+    real(8) :: t12,t23,t13,delta  ! Are the three mixing angles and the CP-violation phase of the mixing matrix
+    real(8) :: m1,m2,m3           ! m1,m2,m3 are the masses of the mass-eogenstates
+    real(8) :: P                  ! P es el momento del neutrino
+    integer :: nu                 ! nu is 1 for neutrinos an 2 for antineutrino    
     real(8) :: Ne                 ! Ne is the electron density
 
-    real(8) :: Hm(3,3)            ! Hm is the Hamiltonian in the mass base
+    real(8) :: t_Hm(3,3)          ! t_Hm is the Hamiltonian in the mass base
     real(8) :: I(3,3)             ! I is the identity matrix
     real(8) :: traceHm,trHm       ! traceHm is the trace of the Hamiltonian-mass
 
-    real(8) :: result1(3,3),result2(3,3),result3(3,3)
-
-    trHm=traceHm(nu,Ne)
-
-    call hamiltonian_mass(Hm,nu,Ne)
+    call totalHamiltonianMassBase(t_Hm,t12,t23,t13,delta,m1,m2,m3,P,nu,Ne)
     call identityMatrix(I)
+    trHm=traceHm(t12,t23,t13,delta,m1,m2,m3,P,nu,Ne)
 
-    T=Hm-((trHm/3.0d0)*I)
+    T=t_Hm-((trHm/3.0d0)*I)
     return
 end subroutine tMatrix
