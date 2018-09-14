bbad6b189fff6b86b5fa987786e3f14050643393
JORGE <jogabell@hotmail.com>
2018-09-14 16:01:50 -0500

    Se mejora la presición de la matriz de mezcla inversa

--- elements_oscillation_model/inverse_mixing_matrix.f90
+++ elements_oscillation_model/inverse_mixing_matrix.f90
@@ -15,13 +15,10 @@ subroutine inverseMixingMatrix(U_1,t12,t23,t13,delta)
     real(8) :: c12,c23,c13        ! Are the cos's os the mixing angles of the oscillation model
     double complex :: arg,exp_delta   
 
-    s12=sin(t12); s23=sin(t23); s13=sin(t13)
-    c12=cos(t12); c23=cos(t23); c13=cos(t13)
-
+    double complex :: U(3,3)      ! U is the mixing matrix of the oscillation model
     
-    U_1(1,1)=c13*c12;                            U_1(2,1)=c13*s12;                        U_1(3,1)=s13*exp(cmplx(0.0d0,-delta))
-    U_1(1,2)=-s12*c23-c12*s23*s13*exp(cmplx(0.0d0,delta)); U_1(2,2)=c12*c23-s12*s23*s13*exp(cmplx(0.0d0,delta));  U_1(3,2)=s23*c13
-    U_1(1,3)=s12*s23-c12*c23*s13*exp(cmplx(0.0d0,delta));  U_1(2,3)=-c12*s23-s12*c23*s13*exp(cmplx(0.0d0,delta)); U_1(3,3)=c23*c13    
-    !print*, 'U_1(1,3)', U_1(1,3)
+    call mixingMatrix(U,t12,t23,t13,delta)
+    call inverseMatrix(U,U_1)    
+    
     return
 end subroutine inverseMixingMatrix
